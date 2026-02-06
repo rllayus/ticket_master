ad3b529e27f76910068b111b16a7f2ab505370ba
Ricardo Laredo <ricardo@stereum.tech>
2026-02-05 20:38:39 -0400

    Ejemplo de recibir json

--- src/main/java/edu/upb/tickmaster/httpserver/EchoPostHandler.java
+++ src/main/java/edu/upb/tickmaster/httpserver/EchoPostHandler.java
@@ -4,6 +4,7 @@
  */
 package edu.upb.tickmaster.httpserver;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
@@ -31,39 +32,29 @@ public class EchoPostHandler implements HttpHandler {
     public void handle(HttpExchange he) throws IOException {
 
         try {
-            InputStreamReader isr = new InputStreamReader(he.getRequestBody(), StandardCharsets.UTF_8);
             String response;
-            BufferedReader br = new BufferedReader(isr);
             Headers responseHeaders = he.getResponseHeaders();
             responseHeaders.add("Access-Control-Allow-Origin", "*");
             responseHeaders.add("Content-type", ContentType.JSON.toString());
 
             if (he.getRequestMethod().equals("POST")) {
-                try try (InputStream is = exchange.getRequestBody();
-             OutputStream os = exchange.getResponseBody()) {
+                try (InputStream is = he.getRequestBody()) {
 
                     // Read request body
-            Scanner scanner = new Scanner(is, StandardCharsets.UTF_8.name());
-            String requestBody = scanner.useDelimiter("\\A").hasNext() ? scanner.next() : "";
+                    Scanner scanner = new Scanner(is, StandardCharsets.UTF_8.name());
+                    String requestBody = scanner.useDelimiter("\\A").hasNext() ? scanner.next() : "";
 
-            // Parse JSON
-            JsonObject jsonRequest = new com.google.gson.JsonParser().parse(requestBody).getAsJsonObject();
-            String codigo = jsonRequest.get("codigo").getAsString();
+                    // Parse JSON
+                    JsonObject jsonRequest = new com.google.gson.JsonParser().parse(requestBody).getAsJsonObject();
+                    String codigo = jsonRequest.get("codigo").getAsString();
 
-            // Insert into database
+                    // Insert into database
 
-            // Send response
-            JsonObject jsonResponse = new JsonObject();
-            jsonResponse.addProperty("status", "OK");
-            jsonResponse.addProperty("llave_privada", llavePrivada);
-            String response = jsonResponse.toString();
+                    // Send response
+                    JsonObject jsonResponse = new JsonObject();
+                    jsonResponse.addProperty("status", "OK");
+                    response = jsonResponse.toString();
 
-
-                    JsonObject object = new JsonObject();
-                    object.addProperty("nombre", "Ricardo");
-                    object.addProperty("Apellido", "Laredo");
-                    object.addProperty("Apellido2", "Laredos");
-                    response = object.toString();
                     he.sendResponseHeaders(Integer.parseInt(Status._200.name().substring(1, 4)), response.length());
                 } catch (Exception e) {
                     response = "{\"status\": \"NOK\",\"message\": \"No se logro imprir la factura\"}";
@@ -74,13 +65,26 @@ public class EchoPostHandler implements HttpHandler {
                 os.write(response.getBytes());
                 os.close();
                 return;
-            }
+
             }
             if (he.getRequestMethod().equals("GET")) {
 
-                response = "{\"status\": \"NOK\",\"message\": \"No se logro imprir la factura\"}";
+                //response = "{\"status\": \"NOK\",\"message\": \"No se logro imprir la factura\"}";
 
-                byte [] byteResponse = response.getBytes(StandardCharsets.UTF_8);
+                JsonArray jsonResponse = new JsonArray();
+
+                JsonObject jsonResponseObject = new JsonObject();
+                jsonResponseObject.addProperty("status", "OK");
+                jsonResponseObject.addProperty("message", "No se logro imprir la factura");
+                jsonResponse.add(jsonResponseObject);
+
+                jsonResponseObject.addProperty("status2", "OK");
+                jsonResponseObject.addProperty("message2", "No se logro imprir la factura");
+                jsonResponse.add(jsonResponseObject);
+
+                response = jsonResponse.toString();
+
+                byte[] byteResponse = response.getBytes(StandardCharsets.UTF_8);
                 he.sendResponseHeaders(Integer.parseInt(Status._200.name().substring(1, 4)), byteResponse.length);
                 OutputStream os = he.getResponseBody();
                 os.write(byteResponse);
